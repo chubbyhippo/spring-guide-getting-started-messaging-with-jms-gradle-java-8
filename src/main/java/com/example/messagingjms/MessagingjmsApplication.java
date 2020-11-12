925d9ea0e8623b982bf9892569d3f92cfdee99a3
ch <mkmsacc@outlook.com>
2020-11-12 20:00:50 +0700

    First commit

---
+++ src/main/java/com/example/messagingjms/MessagingjmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.messagingjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingjmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingjmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/messagingjms/MessagingjmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messagingjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingjmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
