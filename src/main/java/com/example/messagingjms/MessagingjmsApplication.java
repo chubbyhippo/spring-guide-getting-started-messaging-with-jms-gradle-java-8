c3a2940141e45e4dc377884ac6f76816f3c283cc
ch <mkmsacc@outlook.com>
2020-11-12 20:30:20 +0700

    Create jms send and receive

--- src/main/java/com/example/messagingjms/MessagingjmsApplication.java
+++ src/main/java/com/example/messagingjms/MessagingjmsApplication.java
@@ -1,13 +1,52 @@
 package com.example.messagingjms;
 
+import javax.jms.ConnectionFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
 
 @SpringBootApplication
+@EnableJms
 public class MessagingjmsApplication {
 
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(
+			ConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+
+		configurer.configure(factory, connectionFactory);
+		return factory;
+	}
+
+	@Bean
+	public MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(MessagingjmsApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication
+				.run(MessagingjmsApplication.class, args);
+
+		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+
+		System.out.println("Sending an email message.");
+		jmsTemplate.convertAndSend("mailbox",
+				new Email("info@example.com", "hello"));
 	}
 
 }
